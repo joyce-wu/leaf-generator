f30ba4d3b0b1fa0ca98f64ddb642d63c4f90a7dc
Lance Tan <tanlancekai@gmail.com>
2021-12-20 23:31:49 -0500

    bee interaction with beehives

--- lsystem.py
+++ lsystem.py
@@ -18,11 +18,11 @@ bl_info = {
 
 import bpy
 import bmesh
-from mathutils import Vector, Matrix
+from mathutils import Vector, Matrix, Euler
 from math import *
 import random
 
-SCENE_SIZE = 50
+SCENE_SIZE = 100
 class TreeProperties(bpy.types.PropertyGroup):
     leaf_types = [('1', 'Ovate', 'Ovate'), ('2', 'Linear', 'Linear'), ('3', 'Cordate', 'Cordate'), ('4', 'Maple', 'Maple'), ('5', 'Palmate', 'Palmate'), ('6', 'Spiky Oak', 'Spiky Oak'), ('7', 'Rounded Oak', 'Rounded Oak'), ('8', 'Elliptic', 'Elliptic'), ('9', 'Rectangle', 'Rectangle'), ('10', 'Triangle', 'Triangle')]
     
@@ -30,9 +30,9 @@ class TreeProperties(bpy.types.PropertyGroup):
     leaf_bend : bpy.props.FloatProperty(name="Bend", default=90, min=0, max=360)
     leaf_scale : bpy.props.FloatProperty(name="Scale", default=0.8)
     leaf_branch_angle : bpy.props.IntProperty(name="Leaf Branch Angle", default=41)
-    branch_length : bpy.props.FloatProperty(name="Length", default=0.5, min=0.01)
+    branch_length : bpy.props.FloatProperty(name="Length", default=1.0, min=0.01)
     branch_length_scale : bpy.props.FloatProperty(name="Length Scale", default=1.1)
-    branch_thickness : bpy.props.FloatProperty(name="Thickness", default=0.01)
+    branch_thickness : bpy.props.FloatProperty(name="Thickness", default=0.1)
     branch_angle : bpy.props.FloatProperty(name="Angle", default=35, min=0, max=360)
     n_iter : bpy.props.IntProperty(name="Level Count", default=1, min=1, max=5)
     tropism : bpy.props.FloatVectorProperty(name="Tropism", default=(0, 0, -1), size=3)
@@ -93,8 +93,7 @@ class LSystem:
 
         return lstring
     
-    def draw_lstring(lstring, **params):
-        pos = Vector([random.randrange(-SCENE_SIZE/2, SCENE_SIZE/2), random.randrange(-SCENE_SIZE/2, SCENE_SIZE/2), 0])
+    def draw_lstring(lstring, pos, **params):
         turtle = Turtle(pos=pos, **params)
         for lnode in lstring:
             l, params = lnode.l, lnode.params
@@ -323,58 +322,12 @@ class Field:
         ps.use_advanced_hair = True
         ps.render_type = 'OBJECT'
         ps.instance_object = bpy.data.objects["Blade"]
-        ps.count = 10000
+        ps.count = 20000
         ps.particle_size = 0.075
         ps.size_random = 0.5
         ps.use_rotations = True
         ps.rotation_factor_random = 0.2
         ps.rotation_mode = 'GLOB_Y'
-    
-class TreeGen(bpy.types.Operator):
-    bl_idname = "object.tree_gen"
-    bl_category = "Tree Generator"
-    bl_label = "Generate Tree"
-    bl_options = {'REGISTER'}
-    
-    def execute(self, context):
-        Field.draw()
-        mytool = context.scene.my_tool
-        params = {
-            'n_iter' : mytool.n_iter, # number of iterations
-            'length' : mytool.branch_length, # scales lengths of all branches
-            'length_scale' : mytool.branch_length_scale, # scales lengths of lower-order branches relative to higher-order ones
-            'thickness' : mytool.branch_thickness, # scales thicknesses of all branches
-            # thickness_scale is not paramaterized since it depends on branching rules
-            'branch_angle' : mytool.branch_angle, # branching angle, in degrees
-            'leaf_angle' : mytool.leaf_branch_angle, # Angle between leaf and branch
-            'leaf_scale' : mytool.leaf_scale, # Scaling factor for leaf
-            'leaf_bend' : mytool.leaf_bend, # Bend angle for leaf
-            'leaf_type': int(mytool.leaf_type) - 1, # leaf type
-            'tropism' : mytool.tropism, # direction to bend branches towards
-            'tropism_scale' : mytool.tropism_scale, # strength of bending force
-            'seed' : mytool.seed # random seed
-        }
-        
-        axiom = LSystem.parse_lstring("!({thickness})F({length2})A".format(**params, length2=params['length']*2))
-
-        rules = {
-            "A" : LSystem.parse_lstring(
-                "!({th})?F({length})[&({branch_angle})F({length})A]/(94)[&({branch_angle})F({length})A]/(132.63)[&({branch_angle})F({length})A]".format(**params, th=params['thickness']*1.73)),
-            "F" : [lambda F: LNode("F", F.params[0] * params['length_scale'])],
-            "!" : [lambda n: LNode("!", n.params[0] * 1.7)]
-        }
-        lstring = LSystem.generate_lstring(axiom, rules, params['n_iter'])
-        
-        # second pass--add leaves
-        leaf_rules = {
-            "A" : LSystem.parse_lstring("?[&({leaf_angle})L]/(120)[&({leaf_angle})L]/(120)[&({leaf_angle})L]".format(**params))
-        }
-        lstring = LSystem.generate_lstring(lstring, leaf_rules, 1)
-        
-        random.seed(params['seed'])
-        for num_flowers in range(200):
-            LSystem.draw_lstring(lstring, **params)  
-        return {'FINISHED'}
 
 def leaf_shape(t):
     return [
@@ -618,6 +571,7 @@ def leaf_shape(t):
         ),
     ][t]
 
+
 class TreePanel(bpy.types.Panel):
     bl_label = "Tree Generator"
     bl_idname = "OBJECT_PT_Tree"
@@ -656,7 +610,401 @@ class TreePanel(bpy.types.Panel):
         box.prop(mytool, "branch_angle")
         
         layout.operator(TreeGen.bl_idname)
+
+
+### ==== BEGIN BOIDS STUFF ==== ###
+
+class Boid:
+    STATE_FLOCKING = "flocking"
+    STATE_SEEKING = "seeking"
+    STATE_WAITING = "waiting"
+    
+    def __init__(self, name, p, v):
+        self.name = name
+        self.state = Boid.STATE_FLOCKING
+        self.seeking_dest = None
         
+        self.p = p # position
+        self.v = v # velocity vector
+        self.history = [] # array of (state, p, v)
+        self.history.append( (self.state, self.p.copy(), self.v.copy()) )        
+
+    def draw(self, params):
+        # draw boid
+        bpy.ops.mesh.primitive_cone_add(
+            radius1=1,
+            radius2=0,
+            depth=2,
+            enter_editmode=False,
+            align='WORLD',
+            location=(0, 0, 0), 
+            scale=(1, 1, 1)
+        )
+        bpy.context.object.name = self.name
+        bpy.context.object.data.name = self.name
+        # bpy.ops.object.shade_smooth()
+        # bpy.data.objects[name].scale = [scale, scale, scale]
+
+        obj = bpy.context.object
+        obj.rotation_mode = 'QUATERNION'
+        
+        # add wings
+        right_wing = gen_right_wing()
+        right_verts = right_wing[0]
+        right_faces = right_wing[1]
+        mesh = bpy.data.meshes.new(name="Wing")
+        mesh.from_pydata(right_verts, [], right_faces) 
+        rwing_obj = bpy.data.objects.new(self.name+"_RWing", mesh)
+        bpy.context.scene.collection.objects.link(rwing_obj)
+        bpy.context.view_layer.objects.active = rwing_obj
+        rwing_obj.parent = obj
+        rwing_obj.location = Vector((0.0, -0.34154030680656433, 0.31113627552986145))
+        rwing_obj.scale = Vector((3, 1, 3))
+        rwing_obj.rotation_euler = Euler((-0.46983131766319275, -0.0, 0.0), 'XYZ')
+        rwing_obj.rotation_mode = "ZXY"
+        
+        left_wing = gen_left_wing()
+        left_verts = left_wing[0]
+        left_faces = left_wing[1]
+        mesh = bpy.data.meshes.new(name="Wing")
+        mesh.from_pydata(left_verts, [], left_faces) 
+        lwing_obj = bpy.data.objects.new(self.name+"_LWing", mesh)
+        bpy.context.scene.collection.objects.link(lwing_obj)
+        bpy.context.view_layer.objects.active = lwing_obj
+        lwing_obj.parent = obj
+        lwing_obj.location = Vector((0.0, -0.34154030680656433, 0.31113627552986145))
+        lwing_obj.scale = Vector((3, 1, 3))
+        lwing_obj.rotation_euler = Euler((-0.46983131766319275, -0.0, 0.0), 'XYZ')
+        lwing_obj.rotation_mode = "ZXY"
+
+        for i, (state, p, v) in enumerate(self.history):
+            t = i * params['animation_step']
+            if t > params['animation_length']:
+                return
+
+            # location
+            obj.location = p
+            obj.keyframe_insert(data_path="location", frame=t)
+
+            # direction
+            # construct h, l, u
+            h = v.normalized()
+            up = Vector([0, 0, 1])
+            l = up.cross(h)
+            l.normalize()
+            u = h.cross(l)
+
+            # turn into angle
+            mat = Matrix([h, l, u])
+            mat.transpose()
+            
+            base_mat = Matrix.Rotation(pi/2, 4, "Y") @ Matrix.Rotation(-pi/2, 4, "Z")
+            base_mat = base_mat.to_3x3()
+            mat = mat @ base_mat
+            
+            quat = mat.to_quaternion()
+            obj.rotation_quaternion = quat
+            obj.keyframe_insert(data_path="rotation_quaternion", frame=t)
+        
+        
+        # animate wings
+        z_rotation = 0
+        rotation_step = 1.5
+        direction = 1
+        for t in range(0, params['animation_length'], 5):
+            
+            if (z_rotation >= 0):
+                direction = -1
+            elif (z_rotation <= -1.25):
+                direction = 1
+        
+            z_rotation += (rotation_step * direction)
+            rwing_obj.rotation_euler.z = z_rotation
+            lwing_obj.rotation_euler.z = -z_rotation
+            lwing_obj.keyframe_insert(data_path="rotation_euler", frame=t)
+            rwing_obj.keyframe_insert(data_path="rotation_euler", frame=t)
+
+def boids_init(params):
+    boids = []
+    r = params['territory_radius']
+    s = params['max_speed'] * 0.5
+    while len(boids) < params['count']:
+        p = []
+        v = []
+        for i in range(3):
+            p.append(random.random() * r - r/2)
+            v.append(random.random() * s - s/2)
+        
+        p = Vector(p)
+        v = Vector(v)
+        if p.z <= 0:
+            continue
+        
+        name = "boid_{:04}".format(len(boids))
+        boids.append(Boid(name, p, v))
+
+    print("Created {} boids".format(len(boids)))
+    return boids
+    
+
+# return the list of boids that are radius distance or closer to boids[ix]
+def boids_get_neighbors(boids, ix, radius):
+    boid = boids[ix]
+    radius_squared = radius ** 2
+    
+    for i, other_boid in enumerate(boids):
+        if i == ix:
+            continue
+        dist_squared = (other_boid.p - boid.p).length_squared
+        if dist_squared < radius_squared:
+            yield other_boid
+        
+
+# make boids fly towards center of neighbors. modifies boids[ix].v
+def boids_fly_towards_center(boids, params):
+    factor = 0.05 * params['fly_towards_center']
+    
+    for ix in range(len(boids)):
+        if boids[ix].state is Boid.STATE_FLOCKING:
+            center = Vector([0,0,0])
+            n_neighbors = 0
+            
+            for neighbor in boids_get_neighbors(boids, ix, params['visual_range']):
+                center += neighbor.p
+                n_neighbors += 1
+            
+            if n_neighbors:
+                center = center / n_neighbors
+                boids[ix].v += (center - boids[ix].p) * factor
+
+# make boids avoid others that are too close. modifies boids[ix].v
+def boids_avoid_collisions(boids, params):
+    factor = 0.05 * params['avoid_collisions']
+    dv = Vector([0,0,0])
+    
+    for ix in range(len(boids)):
+        if boids[ix].state is Boid.STATE_FLOCKING:
+            for neighbor in boids_get_neighbors(boids, ix, params['collision_radius']):
+                dv += (boids[ix].p - neighbor.p)
+            boids[ix].v += dv * factor
+
+# make boids match velocity of neighbors. modifies boids[ix].v
+def boids_match_velocity(boids, params):
+    factor = 0.05 * params['match_velocity']
+    
+    for ix in range(len(boids)):
+        if boids[ix].state is Boid.STATE_FLOCKING:
+            avg_v = Vector([0,0,0])
+            n_neighbors = 0
+            
+            for neighbor in boids_get_neighbors(boids, ix, params['visual_range']):
+                avg_v += neighbor.v
+                n_neighbors += 1
+            
+            if n_neighbors:
+                avg_v = avg_v / n_neighbors
+                boids[ix].v += (avg_v - boids[ix].v) * factor
+
+# limit boids speed
+def boids_limit_speed(boids, params):
+    for boid in boids:
+        s_sq = boid.v.length_squared
+        if s_sq > params['max_speed']**2:
+            boid.v.normalize()
+            boid.v = boid.v * params['max_speed']
+            print("limit speed")
+
+
+# keep boids in territory
+def boids_stay_in_territory(boids, params):
+    factor = 1.0 * params['stay_in_territory'] * params['max_speed']/10
+    r = params['territory_radius']
+    margin = 5
+    r -= margin
+    
+    for boid in boids:
+        if boid.state is Boid.STATE_FLOCKING:
+            
+            dist = boid.p - params['territory_center']
+            if dist.length_squared > params['territory_radius']**2:
+                dv = dist.normalized()
+                boid.v -= dv * factor
+        
+            if boid.p.z < margin:
+                boid.v.z += factor
+
+def gen_right_wing():
+    return (
+        [#vectors
+            Vector([0, 0, 0]), 
+            Vector([0.2, 0, 0.2]),
+            Vector([0.35, 0, 0.15]),
+            Vector([0.35, 0, 0.05]),
+            Vector([0.3, 0, -0.05]),
+            Vector([0.05, 0, -0.15]),
+            Vector([0.3, 0, -0.25]),
+            Vector([0.35, 0, -0.4]),
+            Vector([0.2, 0, -0.55]),
+            Vector([0, 0, -0.3]),
+        ],
+        [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]], #faces
+    )
+
+def gen_left_wing():
+    return (
+        [#vectors
+            Vector([0, 0, 0]), 
+            Vector([-0.2, 0, 0.2]),
+            Vector([-0.35, 0, 0.15]),
+            Vector([-0.35, 0, 0.05]),
+            Vector([-0.3, 0, -0.05]),
+            Vector([-0.05, 0, -0.15]),
+            Vector([-0.3, 0, -0.25]),
+            Vector([-0.35, 0, -0.4]),
+            Vector([-0.2, 0, -0.55]),
+            Vector([0, 0, -0.3]),
+        ],
+        [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]], #faces
+    )
+
+def create_boids(params):
+    x = SCENE_SIZE/2 - 10
+    beehive_pos = [
+        Vector((x, x, 0)),
+        Vector((x, -x, 0)),
+        Vector((-x, x, 0)),
+        Vector((-x, -x, 0))
+    ]
+         
+    boids = boids_init(params)
+
+    for i in range(0, params['animation_length'], params['animation_step']):
+        # print("=== STEP {}".format(i))
+        boids_fly_towards_center(boids, params)
+        boids_avoid_collisions(boids, params)
+        boids_match_velocity(boids, params)
+        boids_limit_speed(boids, params)
+        boids_stay_in_territory(boids, params)
+        
+        # state transitions
+        for boid in boids:
+            if boid.state is Boid.STATE_FLOCKING and random.random() < .05:
+                # print("Boid {} is now seeking".format(boid.name))
+                # make it go to a beehive
+                boid.state = Boid.STATE_SEEKING
+                dest = random.choice(beehive_pos).copy()
+                new_v = dest - boid.p
+                new_v.normalize()
+                boid.v = new_v * boid.v.length
+                boid.seeking_dest = dest
+            elif boid.state is Boid.STATE_WAITING and random.random() < 0.10:
+                # print("Boid {} is now flocking".format(boid.name))
+                boid.state = Boid.STATE_FLOCKING
+                boid.seeking_dest = None
+                s = params['max_speed'] * 0.25
+                boid.v = Vector(random.uniform(-s, s) for _ in range(3))
+                
+            elif boid.state is Boid.STATE_SEEKING:
+                new_v = boid.seeking_dest - boid.p
+                new_v.normalize()
+                boid.v = new_v * boid.v.length
+                
+                d2 = (boid.p - boid.seeking_dest).length_squared
+                if d2 < boid.v.length_squared:
+                    # print("Boid {} is now waiting".format(boid.name))
+                    boid.state = Boid.STATE_WAITING
+                    boid.p = boid.seeking_dest
+            
+            # save new pos    
+            if boid.state is not Boid.STATE_WAITING:
+                boid.p += boid.v
+                
+            boid.p.z = max(boid.p.z, 0)
+            boid.history.append( (boid.state, boid.p.copy(), boid.v.copy()) )
+    
+    print("Pathing boids done")
+    for boid in boids:
+        boid.draw(params)
+
+### === MAIN PANEL === ### 
+    
+class TreeGen(bpy.types.Operator):
+    bl_idname = "object.tree_gen"
+    bl_category = "Tree Generator"
+    bl_label = "Generate Tree"
+    bl_options = {'REGISTER'}
+    
+    def execute(self, context):
+        print("Growing grass....")
+        Field.draw()
+        
+        print("Planting flowers.....")
+        
+        mytool = context.scene.my_tool
+        params = {
+            'n_iter' : mytool.n_iter, # number of iterations
+            'length' : mytool.branch_length, # scales lengths of all branches
+            'length_scale' : mytool.branch_length_scale, # scales lengths of lower-order branches relative to higher-order ones
+            'thickness' : mytool.branch_thickness, # scales thicknesses of all branches
+            # thickness_scale is not paramaterized since it depends on branching rules
+            'branch_angle' : mytool.branch_angle, # branching angle, in degrees
+            'leaf_angle' : mytool.leaf_branch_angle, # Angle between leaf and branch
+            'leaf_scale' : mytool.leaf_scale, # Scaling factor for leaf
+            'leaf_bend' : mytool.leaf_bend, # Bend angle for leaf
+            'leaf_type': int(mytool.leaf_type) - 1, # leaf type
+            'tropism' : mytool.tropism, # direction to bend branches towards
+            'tropism_scale' : mytool.tropism_scale, # strength of bending force
+            'seed' : mytool.seed # random seed
+        }
+        
+        axiom = LSystem.parse_lstring("!({thickness})F({length2})A".format(**params, length2=params['length']*2))
+
+        rules = {
+            "A" : LSystem.parse_lstring(
+                "!({th})?F({length})[&({branch_angle})F({length})A]/(94)[&({branch_angle})F({length})A]/(132.63)[&({branch_angle})F({length})A]".format(**params, th=params['thickness']*1.73)),
+            "F" : [lambda F: LNode("F", F.params[0] * params['length_scale'])],
+            "!" : [lambda n: LNode("!", n.params[0] * 1.7)]
+        }
+        lstring = LSystem.generate_lstring(axiom, rules, params['n_iter'])
+        
+        # second pass--add leaves
+        leaf_rules = {
+            "A" : LSystem.parse_lstring("?[&({leaf_angle})L]/(120)[&({leaf_angle})L]/(120)[&({leaf_angle})L]".format(**params))
+        }
+        lstring = LSystem.generate_lstring(lstring, leaf_rules, 1)
+        
+        random.seed(params['seed'])
+        flower_locations = []
+        for num_flowers in range(100):
+            pos = Vector([random.randrange(-SCENE_SIZE/2, SCENE_SIZE/2), random.randrange(-SCENE_SIZE/2, SCENE_SIZE/2), 0])
+            LSystem.draw_lstring(lstring, pos, **params) 
+            flower_locations.append(pos)
+            
+        print("Hatching bees...")
+        boid_params = {
+            # paramaterize these
+            'count' : 50,               # number of boids
+            'visual_range' : 7.5,       # radius of vision for each boid
+            'collision_radius' : 2.0,   # collision radius
+            'fly_towards_center' : 1.0, # weights for boid behavior rules
+            'avoid_collisions' : 1.0,
+            'match_velocity' : 1.0,
+            'stay_in_territory' : 1.0,
+            'seed' : 123456,            # Random seed
+
+            # dont paramaterize these
+            'animation_step' : 5,
+            'animation_length' : 300 * 5,
+            'max_speed': 35.0,
+            'territory_center' : Vector([0,0,10]),
+            'territory_radius' : 40,
+        }
+        
+        create_boids(params=boid_params)
+        
+        print("Done!")
+        return {'FINISHED'}
+
 classes = [TreeProperties, TreePanel, TreeGen, Leaf, Branch]
 
 def register():
